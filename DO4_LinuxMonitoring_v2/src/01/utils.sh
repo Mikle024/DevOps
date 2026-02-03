8431b2d375ba087baa92d972b6c594b59e8f4b03
Onion Yasmin <onionyas@student.21-school.ru>
2026-02-03 15:40:18 +0300

    task 1 done

---
+++ DO4_LinuxMonitoring_v2/src/01/utils.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+ONE_GB_IN_KB=1048576
+
+check_free_space() {
+  AVAILABLE_SPACE_KB=$(df / | awk 'NR==2 {print $4}')
+
+  if [ "$AVAILABLE_SPACE_KB" -le "$ONE_GB_IN_KB" ]; then
+    echo "Attention: There is less than 1 GB left in the file system. The script has been stopped."
+    echo "$(date '+%Y-%m-%d %H:%M:%S') | STOP | Free space less than 1GB" >> "$LOG_FILE"
+    exit 0
+  fi
+}
+
+generate_name() {
+  local base_letters=$1
+  local min_length=$2
+
+  local new_name=$base_letters
+  local last_char=${base_letters: -1}
+
+  while [ ${#new_name} -lt "$min_length" ]; do
+      new_name+=$last_char
+  done
+
+  echo "$new_name"
+}
+
+create_folder() {
+  local path_to_create=$1
+  if mkdir -p "$path_to_create"; then
+    echo "Folder created: $path_to_create"
+    echo "$(date '+%Y-%m-%d %H:%M:%S') | FOLDER | $path_to_create" >> "$LOG_FILE"
+    return 0
+  else
+    echo "Error: Failed to create the $path_to_create folder"
+    echo "$(date '+%Y-%m-%d %H:%M:%S') | ERROR | Failed to create folder $path_to_create" >> "$LOG_FILE"
+    return 1
+  fi
+}
+
+create_file() {
+  local file_path=$1
+  local file_size=$2
+  if fallocate -l "${file_size}K" "$file_path"; then
+    echo " - A file has been created: $file_path"
+    echo "$(date '+%Y-%m-%d %H:%M:%S') | FILE | $file_path | ${file_size}kb" >> "$LOG_FILE"
+    return 0
+  else
+    echo " - Error: The $file_path file could not be created"
+    echo "$(date '+%Y-%m-%d %H:%M:%S') | ERROR | Failed to create file $file_path" >> "$LOG_FILE"
+    return 1
+  fi
+}
