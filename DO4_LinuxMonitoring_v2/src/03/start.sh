e62ea2ed85c5b6b27d610f51a7f9c4cba9565bdc
Onion Yasmin <onionyas@student.21-school.ru>
2026-05-11 20:40:14 +0300

    feat: done task 03, modified start.sh

--- DO4_LinuxMonitoring_v2/src/03/start.sh
+++ DO4_LinuxMonitoring_v2/src/03/start.sh
@@ -8,7 +8,7 @@ fi
 
 if [ $# -ne 1 ]; then
   echo "Error: Exactly one parameter is required (1, 2, or 3)."
-  echo "Usage: $0 [1/2/3]"
+  echo "Usage: sudo $0 [1/2/3]"
   echo "1 - Cleaning by log file"
   echo "2 - Cleaning by date and time"
   echo "3 - Cleaning by name mask"
